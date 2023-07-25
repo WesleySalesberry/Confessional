63a6f794a75d1f297f1799109408f0887bb5a2e6
Wes <wesleySalesberry@hotmail.com>
2023-07-25 13:30:36 -0400

    Updated security and moved file

--- frontend/app/component/Form.tsx
+++ frontend/app/component/Form.tsx
@@ -36,12 +36,13 @@ const Form = () => {
         if (data.error) {
           setError(data.error)
         }
-
         setMessage(data.data);
         updateState(true);
+        console.warn(data.error)
       })
       .catch(error => {
         setError(`Error while adding your confession:  ${error}`)
+        console.error(error);
         console.error(error);
       });
   }
@@ -55,6 +56,7 @@ const Form = () => {
 
     setTimeout(() => {
       setMessage('')
+      setError('')
       setError('')
     }, 3000)
   };
