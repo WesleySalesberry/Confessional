cd30ac626b8ed6bc8df3684cee393c8751ecf9d5
WesleySalesberry <61122432+WesleySalesberry@users.noreply.github.com>
2023-07-24 22:06:58 -0400

    Update (#17)
    * Moved files
    
    * Frontend (#10)
    
    * Added darkmode state
    
    * Added a package
    
    * Frontend (#11)
    
    * Added darkmode state
    
    * Added a package
    
    * Added class to tailwind config
    
    * Frontend (#12)
    
    * Added darkmode state
    
    * Added a package
    
    * Added class to tailwind config
    
    * Frontend (#13)
    
    * Added darkmode state
    
    * Added a package
    
    * Added class to tailwind config
    
    * Added context to the component
    
    * Frontend (#15)
    
    * Added darkmode state
    
    * Added a package
    
    * Added class to tailwind config
    
    * Added context to the component
    
    * Updated version
    
    * Updated import paths
    
    * Update (#14)
    
    * Frontend (#10)
    
    * Added darkmode state
    
    * Added a package
    
    * Frontend (#11)
    
    * Added darkmode state
    
    * Added a package
    
    * Added class to tailwind config
    
    * Frontend (#12)
    
    * Added darkmode state
    
    * Added a package
    
    * Added class to tailwind config
    
    * Frontend (#13)
    
    * Added darkmode state
    
    * Added a package
    
    * Added class to tailwind config
    
    * Added context to the component
    
    * Frontend (#16)
    
    * Added darkmode state
    
    * Added a package
    
    * Added class to tailwind config
    
    * Added context to the component
    
    * Updated version
    
    * Updated import paths
    
    * Update (#14)
    
    * Frontend (#10)
    
    * Added darkmode state
    
    * Added a package
    
    * Frontend (#11)
    
    * Added darkmode state
    
    * Added a package
    
    * Added class to tailwind config
    
    * Frontend (#12)
    
    * Added darkmode state
    
    * Added a package
    
    * Added class to tailwind config
    
    * Frontend (#13)
    
    * Added darkmode state
    
    * Added a package
    
    * Added class to tailwind config
    
    * Added context to the component
    
    * Moved files updates
    
    * Minor updates

--- frontend/app/ui/Layout/BaseLayout.tsx
+++ frontend/app/ui/Layout/BaseLayout.tsx
@@ -2,18 +2,14 @@ import React from "react"
 import { SideNav, TopNav } from "..";
 import { useModeContext } from "@/app/context/ModeContext";
 
+
+
 export interface IBaseLayout {
   children: React.ReactNode
 }
 
 const BaseLayout = (props: IBaseLayout) => {
   return (
-    <div className="flex bg-gray-200">
-      <SideNav />
-      <div className="container m-3">
-        {props.children}
-      </div>
-    </div>
     <div className="flex bg-gray-200">
       <SideNav />
       <div className="container m-3">
