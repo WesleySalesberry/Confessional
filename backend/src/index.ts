16f587561df164610ab30f79fc4ca86fe1bf08eb
Wes <wesleySalesberry@hotmail.com>
2023-07-25 13:26:18 -0400

    Updated security in api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,33 @@
 import express from 'express';
-import { json } from 'body-parser';
+import { json, urlencoded } from 'body-parser';
 import dotenv from 'dotenv'
 import cors from 'cors'
+
+import helmet from "helmet";
+import hpp from 'hpp'
+import mongoSanitize from 'express-mongo-sanitize'
 
 import db from '../db';
 
 const app = express();
 app.use(json());
+app.use(urlencoded({ extended: true, limit: '1kb' }));
 dotenv.config();
-app.use(cors())
+app.use(hpp());
+app.use(helmet());
+
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST']
+}
+
+app.use(cors(corsOptions));
+
+app.use(
+  mongoSanitize({
+    replaceWith: '_',
+  }),
+);
 
 const PORT = 3001 || 4001
 
