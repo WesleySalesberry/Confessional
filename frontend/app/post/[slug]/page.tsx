5d8e7c01ee8ba97ae012d20a3acd5b04dd132620
WesleySalesberry <61122432+WesleySalesberry@users.noreply.github.com>
2023-07-26 14:32:31 -0400

    Added single page for the post (#40)

---
+++ frontend/app/post/[slug]/page.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { BaseLayout } from "@/app/ui"
+import React, { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import { TfiThemifyFaviconAlt, TfiEye } from "react-icons/tfi";
+
+import { useParams } from 'next/navigation';
+import { CardProps } from "@/app/ui/NoteCard";
+
+export default function Page() {
+  const route = useRouter();
+  const [data, setData] = useState<CardProps>()
+  const { slug } = useParams();
+
+  console.log(slug)
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/api/v1/confession/${slug}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.data)
+        console.log(data)
+      })
+  }, [])
+
+  return (
+    <BaseLayout>
+      <section className='container bg-white dark:bg-stone-600 rounded'>
+        <div
+          className="my-2 mx-2 py-2 px-2"
+        >
+          <p
+            className="w-20 py-2 px-2 text-sky-500 bg-slate-600 dark:text-sky-500 hover:text-sky-300 dark:hover:text-sky-300 rounded cursor-pointer"
+            onClick={() => route.back()}
+          >
+            Go Back
+          </p>
+        </div>
+        <div className="w-1/2 p-4 mx-auto text-center border-b-2 border-sky-300">
+          <h1
+            className="font-bold text-2xl text-center text-black dark:text-sky-500"
+          >
+            {data?.title}
+          </h1>
+        </div>
+        <div className="w-1/2 p-4 mx-auto text-center">
+          <p className="text-base text-center text-black dark:text-sky-500">{data?.body}</p>
+        </div>
+        <div className="flex justify-between items-center my-5 mx-5 text-gray-500">
+          <div className="flex items-center">
+            <TfiEye
+              className="mr-1 dark:text-emerald-700"
+            /> {data?.views}
+          </div>
+          <div className="flex items-center">
+            <TfiThemifyFaviconAlt
+              className="mr-1 text-indigo-400 dark:text-indigo-700"
+            />
+            <span>{0}</span>
+          </div>
+        </div>
+      </section>
+    </BaseLayout>
+  )
+}
