304840914c575309d64bc8b73d5bea94efe1b7c2
Wes <wesleySalesberry@hotmail.com>
2023-08-10 06:52:25 -0400

    Added pages

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from "react"
+import { BaseLayout } from "../ui";
+
+export default function About() {
+  return (
+    <BaseLayout>
+      <div className="container text-center mx-2 my-2 py-2 px-2">
+        <h1 className="font-xlg font-bold dark:text-white">About Us</h1>
+
+      </div>
+    </BaseLayout>
+  )
+}
